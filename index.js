08cbed14907087471b7a03adcdf80f6b476c8926
David <davidhayriyan@gmail.com>
2017-10-04 00:39:52 +0400

    Add intial express application file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const sql = require('mssql')
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
