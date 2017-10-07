95aa095dd36435a59fcfe1480d7ffe3a801e447a
David <davidhayriyan@gmail.com>
2017-10-07 23:26:52 +0400

    Add functionality to connect mssql server
    Now we can create/delete databases with unique names

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require('express')
 const app = express()
-const sql = require('mssql')
+const Connection = require('tedious').Connection;
+const Request = require('tedious').Request;
+const shortid = require('shortid');
+const formidable = require('formidable');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('./'));
 
-app.listen(3000, function () {
+var config = {
+  userName: 'SA',
+  password: '!HimnarK2RDP#',
+  server: 'localhost'
+}
+
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+app.post('/upload', (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "./uploads";
+  form.maxFieldsSize = 30 * 1024 * 1024;
+  form.parse(req, (err, fields, files) => {
+    res.write('File uploaded');
+    var dbName = shortid.generate();
+    runDBStatement("CREATE", dbName);
+    runDBStatement("DELETE", dbName);
+    res.end();
+  });
+});
+
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
-})
+});
+
+////////////////////////////////////////////////
+// Utilities                                  //
+// TODO: Move into another js file            //
+////////////////////////////////////////////////
+let runDBStatement = (option, dbName) => {
+  var request = new Request(option + " DATABASE " + dbName, function(err, rowCount) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(rowCount + ' rows');
+    }
+    connection.close();
+  });
+  connection.execSql(request);
+}
