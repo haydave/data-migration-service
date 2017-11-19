934857fb1a5b9ab8f2abc994af5bc72c36abe044
David <davidhayriyan@gmail.com>
2017-11-19 23:20:37 +0400

    Generate employee list xml initial version

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const formidable = require('formidable');
 const extract = require('extract-zip')
 const fs = require('fs');
 const path = require('path');
+const xmlBuilder = require('xmlbuilder');
 
 app.use(express.static('./'));
 
 const config = {
   userName: 'SA',
   password: '!HimnarK2RDP#',
-  server: 'localhost'
+  server: 'localhost',
+  options: {
+    rowCollectionOnRequestCompletion: true
+  }
 }
 
 var connection = new Connection(config);
 var isConnected = false;
 connection.on('connect', function(err) {
-  console.log("asdasdsadsad")
   if (err) {
     console.log(err);
   } else {
@@ -52,7 +55,7 @@ app.post('/upload', (req, res) => {
               if (path.extname(files[i]) === '.bak') {
                 var bakFilepath = uploadDir + files[i];
                 if (isConnected) {
-                  restoreDatabase('Salary', dbName, bakFilepath, uploadDir); // We shoudl be sure that all databases of AS have Salay name
+                  restoreDatabase('Salary', dbName, bakFilepath, uploadDir, getEmployeeList); // We shoudl be sure that all databases of AS has Salay name
                 }
                 break;
               }
@@ -82,7 +85,7 @@ var runDBStatement = (option, dbName) => {
   connection.execSql(request);
 }
 
-var restoreDatabase = (logicalName, dbName, bakFile, uploadDir) => {
+var restoreDatabase = (logicalName, dbName, bakFile, uploadDir, callback) => {
   fs.chmodSync(uploadDir, '777'); // hope that this is the temprorary solution, mssql should have write access to uploadDir
   let sql = "RESTORE DATABASE " + dbName + " " +
     "FROM DISK='" + bakFile + "' " +
@@ -92,8 +95,199 @@ var restoreDatabase = (logicalName, dbName, bakFile, uploadDir) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(rowCount + ' rows');
+      callback(dbName)
     }
   });
   connection.execSql(request);
-}
+};
+
+var getEmployeeList = (dbName) => {
+  let sql = 'SELECT * FROM [' + dbName + '].[dbo].[EMPLOYEES] EMP LEFT JOIN [' + dbName + '].[dbo].[ATTACHMENTS] ATCH ON EMP.fEMPLISN = ATCH.fISN;';
+  let request = new Request(sql, (err, rowCount, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      getEmployeeListXML(rows)
+    }
+  });
+  connection.execSql(request);
+};
+
+var getEmployeeListXML = (rows) => {
+  var xmlJSObject = {
+    'Exchange': {
+      '@xmlns': 'http://www.armsoft.am/Accountant/6.0',
+      'Employee': []
+    }
+  };
+
+var employee = {
+    'Code': '',
+    'FullName': '',
+    'Birthday': '',
+    'WGContract': '',
+    'DateOn': '',
+    'InOrderNumber': '',
+    'DateOff': '',
+    'OffOrderNumber': '',
+    'Department': '',
+    'Position': '',
+    'Profession': '',
+    'Gender': '',
+    'SocCardNumber': '',
+    'Phone': '',
+    'MobilePhone': '',
+    'Email': '',
+    'IDDocType': '',
+    'Passport': '',
+    'GivenDate': '',
+    'GivenBy': '',
+    'Nationality': '',
+    'OtherDocumentNumber': '',
+    'DepositAcc': '',
+    'Resident': '',
+    'IncomeTax': '',
+    'SalaryAccount': '',
+    'TradeUnionPercent': '',
+    'RegistrationPlace': '',
+    'Region1': '',
+    'Commune1': '',
+    'City1': '',
+    'Street1': '',
+    'House1': '',
+    'Apartment1': '',
+    'IsRegisteredInSameStay': '',
+    'StayPlace': '',
+    'Country2': '',
+    'Region2': '',
+    'Commune2': '',
+    'City2': '',
+    'Locality': '',
+    'Street2': '',
+    'House2': '',
+    'Apartment2': '',
+    'AddressLine1': '',
+    'AddressLine2': '',
+    'AddressLine3': '',
+    'PostalNumber': '',
+    'EmployeeLanguages': '',
+    'Education': '',
+    'Institute': '',
+    'EduBeginDate': '',
+    'EduEndDate': '',
+    'EduCertificateNumber': '',
+    'MilitaryService': '',
+    'MilitaryServiceBeginDate': '',
+    'MilitaryServiceEndDate': '',
+    'FamilyMember1': '',
+    'FamilyMemberFullName1': '',
+    'FamilyMember2': '',
+    'FamilyMemberFullName2': '',
+    'FamilyMember3': '',
+    'FamilyMemberFullName3': '',
+    'FamilyMember4': '',
+    'FamilyMemberFullName4': '',
+    'MaritalStatus': '',
+    'DatePositionStart': '',
+    'Participation': '',
+    'PartnerCode': '',
+    'EmployerPerc': '',
+    'EmployerSumm': '',
+    'ReducePension': '',
+    'StampFee': '',
+    'EmplFeeHolds': '',
+    'Extension': '',
+    'Image': '',
+    'Attachments': '',
+  };
+  var employeeMap = {
+    'fADDRESSLINE1': 'AddressLine1',
+    'fADDRESSLINE2': 'AddressLine2',
+    'fADDRESSLINE3': 'AddressLine3',
+    'fAPARTMENT1': 'Apartment1',
+    'fAPARTMENT2': 'Apartment2',
+    'fBIRTHDAY': 'Birthday',
+    'fCAPTION': 'FullName',
+    'fCITY1': 'City1',
+    'fCITY2': 'City2',
+    'fCOMMUNE1': 'Commune1',
+    'fCOMMUNE2': 'Commune2',
+    'fCOUNTRY2': 'Country2',
+    'fDATEOFF': 'DateOff',
+    'fDATEON': 'DateOn',
+    'fDEPARTMENT': 'Department',
+    'fDEPOSITACC': 'DepositAcc',
+    'fEDUBEGINDATE': 'EduBeginDate',
+    'fEDUCATION': 'Education',
+    'fEDUCERTIFICATENUMBER': 'EduCertificateNumber',
+    'fEDUENDDATE': 'EduEndDate',
+    'fEMAIL': 'Email',
+    'fEMPLCODE': 'Code',
+    'fEMPLOYEELANGUAGES': 'EmployeeLanguages',
+    'fEMPLOYERPERC': 'EmployerPerc',
+    'fEMPLOYERSUMM': 'EmployerSumm',
+    'fFAMILYMEMBER1': 'FamilyMember1',
+    'fFAMILYMEMBER2': 'FamilyMember2',
+    'fFAMILYMEMBER3': 'FamilyMember3',
+    'fFAMILYMEMBER4': 'FamilyMember4',
+    'fFAMILYMEMBERFULLNAME1': 'FamilyMemberFullName1',
+    'fFAMILYMEMBERFULLNAME2': 'FamilyMemberFullName2',
+    'fFAMILYMEMBERFULLNAME3': 'FamilyMemberFullName3',
+    'fFAMILYMEMBERFULLNAME4': 'FamilyMemberFullName4',
+    'fGIVENBY': 'GivenBy',
+    'fGIVENDATE': 'GivenDate',
+    'fHOUSE1': 'House1',
+    'fHOUSE2': 'House2',
+    'fIDDOCTYPE': 'IDDocType',
+    'fIMAGE': 'Image',
+    'fINCOMETAX': 'IncomeTax',
+    'fINORDERNUM': 'InOrderNumber',
+    'fINSTITUTE': 'Institute',
+    'fISREGSAMESTAY': 'IsRegisteredInSameStay',
+    'fLOCALITY': 'Locality',
+    'fMARITALSTATUS': 'MaritalStatus',
+    'fMILITARYSERVICE': 'MilitaryService',
+    'fMILITARYSERVICEBEGINDATE': 'MilitaryServiceBeginDate',
+    'fMILITARYSERVICEENDDATE': 'MilitaryServiceEndDate',
+    'fMOBILEPHONE': 'MobilePhone',
+    'fNATIONALITY': 'Nationality',
+    'fOFFORDERNUM': 'OffOrderNumber',
+    'fOTHERDOCNUM': 'OtherDocumentNumber',
+    'fPARTICIPATION': 'Participation',
+    'fPARTID': 'PartnerCode',
+    'fPASSPORT': 'Passport',
+    'fPHONE': 'Phone',
+    'fPOSITION': 'Position',
+    'fPOSITIONSTART': 'DatePositionStart',
+    'fPOSTALNUMBER': 'PostalNumber',
+    'fPROFESSION': 'Profession',
+    'fREDUCEPENSION': 'ReducePension',
+    'fREGION1': 'Region1',
+    'fREGION2': 'Region2',
+    'fREGPLACE': 'RegistrationPlace',
+    'fRESIDENT': 'Resident',
+    'fSALARYACCOUNT': 'SalaryAccount',
+    'fSEX': 'Gender',
+    'fSOCCARDNUM': 'SocCardNumber',
+    'fSTAMPFEE': 'StampFee',
+    'fSTAYPLACE': 'StayPlace',
+    'fSTREET1': 'Street1',
+    'fSTREET2': 'Street2',
+    'fTRADEUNIONPERC': 'TradeUnionPercent',
+    'fWGCONTRACT': 'WGContract'
+  }
+  rows.forEach((row) => {
+    var employeeInstance = JSON.parse(JSON.stringify(employee));
+    row.forEach((column) => {
+      var value = column.value;
+      if (column.metadata.type.type === 'DATETIMN' && column.value) {
+        value = value.toISOString().replace(/\..+/, '');
+      }
+      employeeInstance[employeeMap[column.metadata.colName]] = value;
+    });
+    employeeInstance['@xmlns:i'] = 'http://www.w3.org/2001/XMLSchema-instance';
+    xmlJSObject.Exchange.Employee.push(employeeInstance);
+  });
+  xml = xmlBuilder.create(xmlJSObject, { encoding: 'utf-8' }).end({ pretty: true });
+  console.log(xml);
+};
